467af7a5fc306038205b37dd92c8779713b4054c
hunkguo <guoyao@gmail.com>
2021-12-22 10:34:04 +0800

    修改tag

--- pages/agent_bookkeeping.js
+++ pages/agent_bookkeeping.js
@@ -1,6 +1,7 @@
 import styles from '../styles/Home.module.css'
 import Header from './components/hearder'
 import Footer from './components/footer'
+import Image from 'next/image'
 
 
 export default function AgentBookkeeping() {
@@ -8,18 +9,18 @@ export default function AgentBookkeeping() {
         <>
         <Header/>
 
-        <main class="text-gray-400 bg-gray-900 body-font">
-            <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-                <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+        <main className="text-gray-400 bg-gray-900 body-font">
+            <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+                <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
 
-                <p class="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
-                <div class="flex justify-center">
-                    <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg" control-id="ControlID-96">Button</button>
-                    <button class="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">Button</button>
+                <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+                <div ="flex justify-center">
+                    <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg" control-id="ControlID-96">Button</button>
+                    <button className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">Button</button>
                 </div>
                 </div>
-                <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-                <img class="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600"/>
+                <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+                <Image className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600"/>
                 </div>
             </div>
         </main>
