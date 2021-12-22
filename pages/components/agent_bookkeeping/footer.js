cc52683b087531458c62297480498c4e1edbe585
hunkguo <guoyao@gmail.com>
2021-12-22 21:27:45 +0800

    修改文字

--- pages/components/agent_bookkeeping/footer.js
+++ pages/components/agent_bookkeeping/footer.js
@@ -11,7 +11,7 @@ export default function Footer() {
                 <input type="text" id="footer-field" name="footer-field" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:ring-2 focus:bg-transparent focus:ring-indigo-200 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" control-id="ControlID-121"/>
                 </div>
                 <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">提交</button>
-                <p class="text-gray-500 text-sm md:ml-6 md:mt-0 mt-2 sm:text-left text-center">如果您有开发需求，请联系我们。即使您仅仅是来咨询，我们也会感到万分荣幸！
+                <p class="text-gray-500 text-sm md:ml-6 md:mt-0 mt-2 sm:text-left text-center">如果您有业务需求，请联系我们。即使您仅仅是来咨询，我们也会感到万分荣幸！
                 </p>
             </div>
                 <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
